64f36d33293e76f36f683c14809210203cfafed2
kinmy <651951271@qq.com>
2018-03-28 13:58:32 +0800

    7第七章播放器内置组件开发

---
+++ README.md
@@ -0,0 +1,30 @@
+# 第七章 播放器内置组件开发
+```
+vuex
+QQ歌词获取（修改请求头referer）
+css前缀兼容补全
+js钩子插件 create-keyframe-animation
+audio
+补“0”算法
+svg圆形进度条
+shuffle洗牌算法
+base-64插件
+lyric-parse插件
+vue功能mixin
+```
+   
+# Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="singer" ref="singer">
+    <listview @select="selectSinger" :data="singers" ref="list"></listview>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+  import Listview from 'base/listview/listview'
+  import {mapMutations} from 'vuex'
+  import {playlistMixin} from 'common/js/mixin'
+
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
+
+  export default {
+    mixins: [playlistMixin],
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      handlePlaylist(playlist) {
+        const bottom = playlist.length > 0 ? '60px' : ''
+        this.$refs.singer.style.bottom = bottom
+        this.$refs.list.refresh()
+      },
+      selectSinger(singer) {
+        this.$router.push({
+          path: `/singer/${singer.id}`
+        })
+        this.setSinger(singer)
+      },
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = this._normalizeSinger(res.data.list)
+            // console.log(this._normalizeSinger(this.singers))
+          }
+        })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        })
+        // 为了得到有序列表，我们需要处理 map
+        let hot = []
+        let ret = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
+      },
+      ...mapMutations({
+        setSinger: 'SET_SINGER'
+      })
+    },
+    components: {
+      Listview
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
+</style>
