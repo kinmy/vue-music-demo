aa5382f86c74d73ce0f8d2cb735b736640cc67c2
kinmy <651951271@qq.com>
2018-03-31 13:26:00 +0800

    12第十二章 用户中心页面

---
+++ README.md
@@ -0,0 +1,26 @@
+# 第十二章 用户中心页面
+```
+学习内容：
+vuex
+mixin
+```
+```
+失误：
+无
+```
+   
+# Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
+import 'common/stylus/index.styl'
+
+fastclick.attach(document.body)
+
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  store,
+  router
+})
