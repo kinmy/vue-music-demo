43cb718589bea821c75548066a0d1e367564a1df
kinmy <651951271@qq.com>
2018-03-31 18:07:20 +0800

    vue-music-demo-complete

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # vue-music-demo
+
+## vue2.0 + vue-router + vuex + express + jsonp + axios + webpack-ver-3.6
+   
+# Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
---
+++ src/components/singer-detail/singer-detail.vue
@@ -0,0 +1,73 @@
+<template>
+  <transition name="slide">
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
+  </transition>
+</template>
+
+<script type="text/javascript">
+  import {mapGetters} from 'vuex'
+  import {getSingerDetail} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
+
+  export default {
+    data() {
+      return {
+        songs: []
+      }
+    },
+    computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
+      ...mapGetters([
+        'singer'
+      ])
+    },
+    created() {
+      this._getDetail()
+      // console.log(this.singer)
+    },
+    methods: {
+      _getDetail() {
+        if (!this.singer.id) {
+          this.$router.push('/singer')
+          return
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if (res.code === ERR_OK) {
+            // console.log(res.data.list)
+            this.songs = this._normalizeSongs(res.data.list)
+            // console.log(this.songs)
+          }
+        })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((item) => {
+          let {musicData} = item
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
+    },
+    components: {
+      MusicList
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+  @import "~common/stylus/variable"
+
+  .slide-enter-active,.slide-leave-active
+    transition: all 0.3s
+  .slide-enter,.slide-leave-to
+    transform: translate3d(100%,0,0)
+</style>
